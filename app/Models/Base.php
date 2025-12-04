d8efd66d9b519cc8b63d29e7dfed9f1fdf34b6ef
LZonca <leo.zonca@business-school.academy>
2025-12-04 23:39:28 +0100

    Ajout base model

---
+++ app/Models/Base.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Base extends Model
+{
+    protected $fillable = [
+        'name',
+        'created_by',
+        'updated_by'
+    ];
+
+
+}
